ef3d987bf06052546b660ae6fc1d803c85650a48
whk-irl <83494574+whk-irl@users.noreply.github.com>
2022-04-18 10:30:30 -0500

    update

---
+++ Holo-Haptic/Assets/Scripts/SendInfo.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class SendInfo : MonoBehaviour
+{
+
+    SerialPort sp;
+    float next_time;
+    int ii = 0;
+    int x, y, z;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        string the_com = "";
+        next_time = Time.time;
+        x = 1;
+        y = 1;
+        z = 1;
+        Debug.Log("Hello");
+
+        foreach (string mysps in SerialPort.GetPortNames())
+        {
+            print(mysps);
+            if (mysps != "COM1") { the_com = mysps; break; }
+        }
+        sp = new SerialPort("\\\\.\\" + the_com, 115200);
+        if (!sp.IsOpen)
+        {
+            print("Opening " + the_com + ", baud 115200");
+            sp.Open();
+            sp.ReadTimeout = 100;
+            sp.Handshake = Handshake.None;
+            if (sp.IsOpen) { print("Open"); }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > next_time)
+        {
+            if (!sp.IsOpen)
+            {
+                sp.Open();
+                print("opened sp");
+            }
+            if (sp.IsOpen)
+            {
+                print("Writing ");
+                sp.Write("X=" + x.ToString() + "Y=" + y.ToString() + "Z=" + z.ToString());
+            }
+            next_time = Time.time + 5;
+            if (++ii > 9) ii = 0;
+        }
+    }
+}
