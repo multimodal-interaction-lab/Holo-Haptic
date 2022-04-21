2837a2b8f0dbe8bded7f87c27bb10cf8cf17f7f1
Multimodal Interaction Lab <83032264+multimodal-interaction-lab@users.noreply.github.com>
2022-04-21 14:12:16 -0500

    Reads port info from a file

--- Holo-Haptic/Assets/Scripts/SendInfo.cs
+++ Holo-Haptic/Assets/Scripts/SendInfo.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO.Ports;
+using System.IO;
 
 public class SendInfo : MonoBehaviour
 {
@@ -10,6 +11,9 @@ public class SendInfo : MonoBehaviour
     float next_time;
     int ii = 0;
     float x, y, z;
+    public int baudrate = 115200;
+    public bool readFromFile;
+    private bool noPortsFound;
 
 
     // Start is called before the first frame update
@@ -22,21 +26,60 @@ public class SendInfo : MonoBehaviour
         z = transform.position.z;
         Debug.Log("Hello");
 
-        foreach (string mysps in SerialPort.GetPortNames())
+        if (readFromFile)
         {
-            print(mysps);
-            if (mysps != "COM1") { the_com = mysps; break; }
+            the_com = File.ReadAllText("port.ini");
+
+            if (!the_com.StartsWith("COM"))
+            {
+                noPortsFound = true;
+                Debug.Log("No port found!");
+            }
+            else
+            {
+                try
+                {
+                    OpenPort(the_com, baudrate);
+                    noPortsFound = false;
+                }
+                catch (System.Exception e)
+                {
+                    noPortsFound = true;
+                    Debug.Log("Error opening port: " + e.Message);
+                }
+            }
         }
-        sp = new SerialPort("\\\\.\\" + the_com, 115200);
-        if (!sp.IsOpen)
+        else
         {
-            print("Opening " + the_com + ", baud 115200");
-            sp.Open();
-            sp.ReadTimeout = 100;
-            sp.Handshake = Handshake.None;
-            if (sp.IsOpen) { print("Open"); }
+            string[] ports = SerialPort.GetPortNames();
+            Debug.Log(ports);
+            if (ports.Length < 1)
+            {
+                noPortsFound = true;
+                Debug.Log("No port found!");
+            }
+            else
+            {
+                noPortsFound = false;
+                the_com = ports[0];
+                OpenPort(the_com, baudrate);
+            }
+        }
+        Debug.Log(the_com);
+    }
+
+    public void OpenPort(string portname, int baudrate)
+    {
+        sp = new SerialPort(portname, baudrate);
+        sp.Open();
+        sp.ReadTimeout = 100;
+        sp.Handshake = Handshake.None;
+        if (sp.IsOpen) 
+        { 
+            print("Open");
         }
     }
+
 
     // Update is called once per frame
     void Update()
@@ -51,7 +94,7 @@ public class SendInfo : MonoBehaviour
             if (sp.IsOpen)
             {
                 print("Writing ");
-                sp.Write("X=" + x.ToString() + "Y=" + y.ToString() + "Z=" + z.ToString());
+                sp.Write("X=" + x.ToString() + "Y=" + z.ToString() + "Z=" + y.ToString());
             }
             next_time = Time.time + 5;
         }
